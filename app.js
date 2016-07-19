e44ae98dfc9d5f282c4e145f2fdaf0e72c3f2c03
Hitte <hitte@Chriss-MacBook-Pro.local>
2016-07-19 00:10:23 -0600

    completed tutorial: http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Mongodb setup
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('127.0.0.1:27017/mysite');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,52 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+    '/userlist',
+    function(req, res, next) {
+      var db = req.db;
+      var collection = db.get('usercollection')
+      collection.find(
+          {},
+          {},
+          function(e, docs) {
+            res.render('userlist',
+                {"userlist" : docs}
+            )
+          }
+      );
+    }
+);
+
+router.get(
+    '/userdata',
+    function(req, res, next) {
+        res.render('userdata', {"title" : "Create New User"})
+    }
+);
+
+router.post(
+    '/adduser',
+    function(req, res, next) {
+        var db = req.db;
+
+        var username = req.body.username;
+        var useremail = req.body.useremail;
+
+        var collection = db.get('usercollection')
+
+        collection.insert(
+            {"username" : username, "email" : useremail},
+            function(err, doc) {
+                if(err) {
+                    res.send("there was a problem adding the information to the database.");
+                }
+                else {
+                    res.redirect("userlist");
+                }
+            }
+        );
+    }
+);
 
 module.exports = router;
